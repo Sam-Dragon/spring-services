63dc7826fee4052a12855183f8d9e9b5857ff842
rahul.sheth <rahul.sheth@R065.corp.vcti.io>
2022-01-25 12:25:12 +0530

    Added Kafka service

--- user-service/src/main/java/com/example/user/UserServiceImpl.java
+++ user-service/src/main/java/com/example/user/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -8,22 +9,35 @@ import com.example.dto.Department;
 import com.example.dto.UserDepartment;
 import com.example.entity.User;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class UserServiceImpl implements UserService {
+
+	private static final String TOPIC = "message-topic";
 
 	@Autowired
 	private RestTemplate restTemplate;
 
 	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	@Autowired
 	private UserRepository repository;
 
-	// Gateway Service Url
+//	Gateway Service Url
 //	private final String DEPARTMENT_SERVICE = "http://department-service/departments/";
 	private final String DEPARTMENT_SERVICE = "http://localhost:9001/departments/";
 
 	@Override
 	public User saveUser(User user) {
-		return repository.save(user);
+		User registerUser = repository.save(user);
+
+		log.info("Produced Request on Topic :: " + TOPIC + " for user :: " + registerUser.getEmailId());
+		kafkaTemplate.send(TOPIC, registerUser.getEmailId() + " is registered successfully..");
+
+		return registerUser;
 	}
 
 	@Override
